36cc54cb8bb9228dd1e8f42d673e2a8be902b974
DarrenCT <99516347+DarrenCT@users.noreply.github.com>
2024-01-21 22:43:34 -0500

    push front end changes, page layout mostly completed

--- movieClient/movie-reviews-v1/src/api/axiosConfig.js
+++ movieClient/movie-reviews-v1/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:'https://8713-70-48-90-55.ngrok-free.app',
-    headers: {"ngrok-skip-browser-warning": "true"}
+  baseURL: "https://18bb-70-48-90-55.ngrok-free.app",
+  headers: { "ngrok-skip-browser-warning": "true" },
 });
